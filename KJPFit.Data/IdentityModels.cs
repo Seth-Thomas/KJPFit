99e1d2e2d4b757ef82bce91b019b7d36ee1fad08
Seth-Thomas <Seth.b.thomas8@gmail.com>
2021-04-10 14:48:10 -0400

    added dbsets

--- KJPFit.Data/IdentityModels.cs
+++ KJPFit.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -29,5 +31,38 @@ namespace KJPFit.Data
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+        public DbSet<Stat> Stats { get; set; }
+
+        public DbSet<Exercise> Exercises { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+            .Conventions
+            .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+        {
+            public IdentityUserLoginConfiguration()
+            {
+                HasKey(iul => iul.UserId);
+            }
+        }
+        public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+        {
+            public IdentityUserRoleConfiguration()
+            {
+                HasKey(iur => iur.UserId);
+            }
+        }
+
     }
 }
